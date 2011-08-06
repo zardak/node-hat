19f2028175c19cd076037dab854950e1048688a4
James Halliday <mail@substack.net>
2011-08-05 19:49:15 -0700

    hat racks and a test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var hat = module.exports = function (bits, base) {
     if (!base) base = 16;
+    if (bits === undefined) bits = 128;
     if (bits <= 0) return '0';
+    
     var digits = Math.log(Math.pow(2, bits)) / Math.log(base);
     var rem = digits - Math.floor(digits);
     
@@ -21,4 +23,24 @@ var hat = module.exports = function (bits, base) {
         return hat(bits, base)
     }
     else return res;
+};
+
+hat.rack = function (bits, base) {
+    var fn = function () {
+        var iters = 0;
+        do {
+            if (iters ++ > 10) {
+                throw new Error('too many ID collisions, use more bits')
+            }
+            
+            var id = hat(bits, base);
+        } while (Object.hasOwnProperty.call(hats, id));
+        
+        hats[id] = true;
+        return id;
+    };
+    var hats = fn.hats = {};
+    fn.bits = bits || 128;
+    fn.base = base || 16;
+    return fn;
 };
---
+++ test/rack.js
@@ -0,0 +1,18 @@
+var hat = require('../');
+var assert = require('assert');
+
+exports.rack = function () {
+    var rack = hat.rack(4);
+    var seen = {};
+    for (var i = 0; i < 8; i++) {
+        var id = rack();
+        assert.ok(!seen[id], 'seen this id');
+        seen[id] = true;
+        
+        assert.ok(id.match(/^[0-9a-f]$/));
+    }
+    
+    assert.throws(function () {
+        for (var i = 0; i < 10; i++) rack()
+    });
+};
